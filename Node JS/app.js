25109a456bb6890ec6141c5ed5f972b03f1688e3
Asim Nazeer <asimnazeer925@gmail.com>
2025-09-22 15:34:05 +0500

    Event, FS, OS, listners, emit.

--- Node JS/app.js
+++ Node JS/app.js
@@ -1,12 +1,11 @@
-// function sayHello(name) {
-//   console.log("hello", name);
-// }
+// =============================== using function from other file =======================================
 
-// sayHello("Asim");
+/*
+    to get any export define in any module (file.js),
+    we use require to get the "exports obj" of that module: "module.exports"
+    we're saying that exports obj we got is named as logger
+    no need to say logger.js, as its already known as js file
+*/
 
-// to get any export define in any module (file.js), we use require to get the exports of that module: "module.exports"
-
-// we're saying that exports obj we got is named as logger
-const logger = require("./logger"); // no need to say logger.js, as its already known as js file
-
+const logger = require("./logger");
 logger.logFu("Here is the info about Mercedes CLR GTR");
