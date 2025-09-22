3b968de38350c7df66cc20639503bebeab192b05
Asim Nazeer <asimnazeer925@gamil.com>
2025-09-22 15:34:05 +0500

    Event, FS, OS, listners, emit.

--- Node JS/logger.js
+++ Node JS/logger.js
@@ -1,8 +1,15 @@
 var url = "https://mylogger.io/log";
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
 
 function log(message) {
-  // send https req
   console.log(message);
+
+  emitter.on("logging", (arg) => {
+    console.log("logger listner called", arg);
+  });
+
+  emitter.emit("logging", { msg: message }); // raising*
 }
 
 module.exports.logFu = log; // value is log, key is logFu
