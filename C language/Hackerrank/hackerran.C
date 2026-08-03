0c2317c1f75251f6c7711cbb5d5e518f59ba9e82
Asim925 <asimnazeer925@gmail.com>
2026-08-03 12:50:26 +0500

    student marks sum

--- C language/Hackerrank/hackerran.C
+++ C language/Hackerrank/hackerran.C
@@ -229,16 +229,20 @@
 //     return 0;
 // }
 
+// ========================= Student marks sum ========================= //
+
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 
 // Complete the following function.
 
 int marks_summation(int *marks, int number_of_students, char gender)
 {
-    // Write your code here.
+    int sum = 0;
+    for (int i = gender == 'g' ? 1 : 0; i < number_of_students; i += 2)
+    {
+        sum += marks[i];
+    }
+    return sum;
 }
 
 int main()
