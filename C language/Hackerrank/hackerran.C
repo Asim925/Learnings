68fc488fc7f0c4fe3e6eee67ed9e78e03f0b9632
Asim925 <asimnazeer925@gmail.com>
2026-08-03 18:53:53 +0500

    4 stars completed

--- C language/Hackerrank/hackerran.C
+++ C language/Hackerrank/hackerran.C
@@ -294,47 +294,96 @@
 
 // ========================= Digit Frequency ========================= //
 
+// #include <stdio.h>
+// #include <string.h>
+// #include <math.h>
+// #include <stdlib.h>
+
+// int main()
+// {
+
+//     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+//     char s[1000];
+//     scanf("%s", s);
+//     int zero = 0, one = 0, two = 0, three = 0, four = 0, five = 0, six = 0, seven = 0, eight = 0, nine = 0;
+//     for (int i = 0; i < 1000; i++)
+//     {
+//         if (s[i] >= 96)
+//             continue;
+//         else if (s[i] == '\0')
+//             break;
+//         else if (s[i] == '0')
+//             zero++;
+//         else if (s[i] == '1')
+//             one++;
+//         else if (s[i] == '2')
+//             two++;
+//         else if (s[i] == '3')
+//             three++;
+//         else if (s[i] == '4')
+//             four++;
+//         else if (s[i] == '5')
+//             five++;
+//         else if (s[i] == '6')
+//             six++;
+//         else if (s[i] == '7')
+//             seven++;
+//         else if (s[i] == '8')
+//             eight++;
+//         else if (s[i] == '9')
+//             nine++;
+//     }
+
+//     printf("%d %d %d %d %d %d %d %d %d %d", zero, one, two, three, four, five, six, seven, eight, nine);
+
+//     return 0;
+// }
+
+// ========================= Boxes throught tunnel ========================= //
+
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 #include <stdlib.h>
+#define MAX_HEIGHT 41
+
+struct box
+{
+    int length;
+    int width;
+    int height;
+};
+
+typedef struct box box;
+
+int get_volume(box b)
+{
+    return b.length * b.height * b.width;
+}
+
+int is_lower_than_max_height(box b)
+{
+    /**
+     * Return 1 if the box's height is lower than MAX_HEIGHT and 0 otherwise
+     */
+    if (b.height < MAX_HEIGHT)
+        return 1;
+    return 0;
+}
 
 int main()
 {
-
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    char s[1000];
-    scanf("%s", s);
-    int zero = 0, one = 0, two = 0, three = 0, four = 0, five = 0, six = 0, seven = 0, eight = 0, nine = 0;
-    for (int i = 0; i < 1000; i++)
+    int n;
+    scanf("%d", &n);
+    box *boxes = malloc(n * sizeof(box));
+    for (int i = 0; i < n; i++)
     {
-        if (s[i] >= 96)
-            continue;
-        else if (s[i] == '\0')
-            break;
-        else if (s[i] == '0')
-            zero++;
-        else if (s[i] == '1')
-            one++;
-        else if (s[i] == '2')
-            two++;
-        else if (s[i] == '3')
-            three++;
-        else if (s[i] == '4')
-            four++;
-        else if (s[i] == '5')
-            five++;
-        else if (s[i] == '6')
-            six++;
-        else if (s[i] == '7')
-            seven++;
-        else if (s[i] == '8')
-            eight++;
-        else if (s[i] == '9')
-            nine++;
+        scanf("%d%d%d", &boxes[i].length, &boxes[i].width, &boxes[i].height);
     }
-
-    printf("%d %d %d %d %d %d %d %d %d %d", zero, one, two, three, four, five, six, seven, eight, nine);
-
+    for (int i = 0; i < n; i++)
+    {
+        if (is_lower_than_max_height(boxes[i]))
+        {
+            printf("%d\n", get_volume(boxes[i]));
+        }
+    }
     return 0;
 }
